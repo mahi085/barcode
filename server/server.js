571e1fd70e0140b7af10e58a11b8f1d822585159
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2026-03-18 17:52:45 +0530

    cors done

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://reliable-faloodeh-aa2f63.netlify.app"
+      "https://reliable-faloodeh-aa2f63.netlify.app",
+      "http://192.168.1.5:5173"
     ],
     credentials: true,
   })
